185efeee3df40eed0cdb122d97b597398f112372
TTengs <theistengs@gmail.com>
2021-11-29 22:44:47 +0100

    Changed file extension

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/SVGView.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/SVGView.java
@@ -95,9 +95,9 @@ public class SVGView extends AbstractView implements ExportableView {
         LinkedList<InputFormat> inputFormats = new LinkedList<InputFormat>();
         inputFormats.add(new SVGZInputFormat());
         inputFormats.add(new ImageInputFormat(new SVGImageFigure()));
-        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "JPG", "Joint Photographics Experts Group (JPEG)", "jpg", BufferedImage.TYPE_INT_RGB));
-        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "GIF", "Graphics Interchange Format (GIF)", "gif", BufferedImage.TYPE_INT_ARGB));
-        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "PNG", "Portable Network Graphics (PNG)", "png", BufferedImage.TYPE_INT_ARGB));
+        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "JPG", "Joint Photographics Experts Group (JPEG)", new String[] {"jpg", "jpeg", "jpe", "jif", "jfif"}, BufferedImage.TYPE_INT_RGB));
+        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "GIF", "Graphics Interchange Format (GIF)", new String[] {"gif"}, BufferedImage.TYPE_INT_ARGB));
+        inputFormats.add(new ImageInputFormat(new SVGImageFigure(), "WebP", "GG ", new String[] {"webp"}, BufferedImage.TYPE_INT_ARGB));
         inputFormats.add(new PictImageInputFormat(new SVGImageFigure()));
         inputFormats.add(new TextInputFormat(new SVGTextFigure()));
         drawing.setInputFormats(inputFormats);
